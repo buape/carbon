443b7b90e894e3e01e0f6505bdd345df0f3cd73b
Shadow <shadow@buape.com>
2024-04-14 15:31:49 -0500

    .github

--- .pierre/ci/biome.ts
+++
@@ -1,7 +0,0 @@
-import { run } from "pierre"
-
-export const label = "Run Biome"
-
-export default async () => {
-	await run(`biome ci .`)
-}
--- .pierre/ci/typecheck.ts
+++
@@ -1,7 +0,0 @@
-import { run } from "pierre"
-
-export const label = "Typechecks"
-
-export default async () => {
-	await run(`pnpm run typecheck`)
-}
