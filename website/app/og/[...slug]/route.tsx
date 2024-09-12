89a4fbde8cafe19bd92f38f8f90226f423b49769
Shadow <hi@shadowing.dev>
2024-09-12 00:05:13 -0500

    web: add logo to og image

--- website/app/og/[...slug]/route.tsx
+++ website/app/og/[...slug]/route.tsx
@@ -7,6 +7,7 @@ import { utils } from "~/app/source"
 
 const font = readFileSync("./app/og/[...slug]/Rubik-Regular.ttf")
 const fontBold = readFileSync("./app/og/[...slug]/Rubik-Bold.ttf")
+import Logo from "~/public/CarbonLogo.png"
 
 export function GET(
 	_: NextRequest,
@@ -25,30 +26,7 @@ export function GET(
 		primaryTextColor: "rgb(240,240,240)",
 		primaryColor: "rgb(145,234,228)",
 		title: page.data.title,
-		icon: (
-			// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="120"
-				height="120"
-				viewBox="0 0 24 24"
-				stroke="url(#test)"
-				style={{
-					marginLeft: "-4px"
-				}}
-				strokeWidth="1"
-				strokeLinecap="round"
-				fill="rgb(0,0,0,0.8)"
-				strokeLinejoin="round"
-			>
-				<linearGradient id="test" x1="0" y1="0" x2="0" y2="1">
-					<stop stopColor="white" />
-					<stop stopColor="rgb(100,100,100)" offset="100%" />
-				</linearGradient>
-				<rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-				<polyline points="11 3 11 11 14 8 17 11 17 3" />
-			</svg>
-		),
+		icon: <img src={Logo.src} alt="Carbon Logo" className="rounded-lg" />,
 		description: page.data.description,
 		site: "Carbon",
 		fonts: [
