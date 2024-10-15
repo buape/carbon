7797395e977e4ef965c84378613c87b9cfb3cd85
Kodie <apteryx.xyz@gmail.com>
2024-10-16 05:03:01 +1300

    fix: replace bash pattern replacement with sed (#185)

--- website/copy-api.sh
+++ website/copy-api.sh
@@ -56,8 +56,8 @@ process_subpackage() {
             local title="$(kebab_to_title_case "$pkg_name")"
         fi
 
-        escaped_name="${name//@/\\@}"
-        escaped_name="${escaped_name//\//\\/}"
+        escaped_name=$(echo "$name" | sed 's/@/\\@/g')
+        escaped_name=$(echo "$escaped_name" | sed 's/\//\\/g')
         sed -i -e "s/title: \(.*\)/title: \"$escaped_name\"/" "$dir/index.mdx"
         sed -i -e "s/## Index/ /" "$dir/index.mdx"
         echo "{ \"title\": \"$title\" }" >"$dir/meta.json"
