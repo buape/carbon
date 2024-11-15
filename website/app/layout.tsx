309db776b4c7e6e0b45311b189ab15f5b7b1db66
Shadow <hi@shadowing.dev>
2024-11-15 10:51:36 -0600

    add analytics

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -3,6 +3,8 @@ import { RootProvider } from "fumadocs-ui/provider"
 import { Rubik } from "next/font/google"
 import type { ReactNode } from "react"
 import { baseUrl, createMetadata } from "./og/[...slug]/metadata"
+import Script from "next/script"
+import { env } from "~/env.mjs"
 
 const rubik = Rubik({
 	subsets: ["latin"],
@@ -25,6 +27,13 @@ export default function Layout({ children }: { children: ReactNode }) {
 			<body>
 				<RootProvider>{children}</RootProvider>
 			</body>
+			{env.NODE_ENV === "production" ? (
+				<Script
+					defer
+					src="https://stats.b1.buape.com/script.js"
+					data-website-id="c864c489-8576-427a-b229-6ffac6fc1c33"
+				/>
+			) : null}
 		</html>
 	)
 }
---
+++ website/env.mjs
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+	extends: [],
+	shared: {},
+	server: {
+		NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+	},
+	client: {},
+	runtimeEnv: {
+		NODE_ENV: process.env.NODE_ENV,
+	},
+	skipValidation:
+		!!process.env.CI ||
+		!!process.env.SKIP_ENV_VALIDATION ||
+		process.env.npm_lifecycle_event === "lint"
+})
