105a4b0e6fd5e182193db7c26b68ae03f164b59b
Shadow <hi@shadowing.dev>
2025-04-29 21:19:47 -0500

    attempted typing fix for sharded forwarder

--- packages/carbon/src/plugins/gateway-forwarder/ShardedGatewayForwarderPlugin.ts
+++ packages/carbon/src/plugins/gateway-forwarder/ShardedGatewayForwarderPlugin.ts
@@ -1,23 +1,18 @@
-import { ShardingPlugin } from "../sharding/ShardingPlugin.js"
+import {
+	ShardingPlugin,
+	type ShardingPluginOptions
+} from "../sharding/ShardingPlugin.js"
 import type { GatewayForwarderPluginOptions } from "./GatewayForwarderPlugin.js"
 import { GatewayForwarderPlugin } from "./GatewayForwarderPlugin.js"
 
-export interface ShardedGatewayForwarderPluginOptions
-	extends Omit<GatewayForwarderPluginOptions, "shard"> {
-	/**
-	 * If not provided, will use Discord's recommended amount
-	 */
-	totalShards?: number
-	/**
-	 * Specific shard IDs to spawn, if not provided will spawn all shards
-	 */
-	shardIds?: number[]
-	/**
-	 * Maximum number of shards to spawn concurrently
-	 */
-	maxConcurrency?: number
-}
+export type ShardedGatewayForwarderPluginOptions =
+	GatewayForwarderPluginOptions & ShardingPluginOptions
 
 export class ShardedGatewayForwarderPlugin extends ShardingPlugin {
+	// biome-ignore lint/complexity/noUselessConstructor:
+	constructor(options: ShardedGatewayForwarderPluginOptions) {
+		super(options)
+	}
+
 	customGatewayPlugin = GatewayForwarderPlugin
 }
