a141af48c82093f0dcac883df6d9cfe97f3bf672
Shadow <hi@shadowing.dev>
2025-10-10 17:24:52 -0500

    add support for the clientmanager in the adapters

--- packages/carbon/src/adapters/bun/index.ts
+++ packages/carbon/src/adapters/bun/index.ts
@@ -1,17 +1,21 @@
 import Bun from "bun"
 import type { Client } from "../../index.js"
+import type { ClientManager } from "../../plugins/client-manager/ClientManager.js"
 import { createHandler } from "../fetch/index.js"
 
 export type Server = Bun.Server
 export type ServerOptions = Omit<Bun.ServeOptions, "fetch">
 
 /**
- * Creates a server for the client using Bun.serve
- * @param client The Carbon client to create the server for
+ * Creates a server for the client or client manager using Bun.serve
+ * @param client The Carbon client or client manager to create the server for
  * @param options Additional options for the server
  * @returns The Bun.Server instance
  */
-export function createServer(client: Client, options: ServerOptions): Server {
+export function createServer(
+	client: Client | ClientManager,
+	options: ServerOptions
+): Server {
 	const fetch = createHandler(client)
 	return Bun.serve({
 		...options,
--- packages/carbon/src/adapters/node/index.ts
+++ packages/carbon/src/adapters/node/index.ts
@@ -1,17 +1,21 @@
 import * as Hono from "@hono/node-server"
 import type { Client } from "../../index.js"
+import type { ClientManager } from "../../plugins/client-manager/ClientManager.js"
 import { createHandler } from "../fetch/index.js"
 
 export type Server = Hono.ServerType
 export type ServerOptions = Omit<Parameters<typeof Hono.serve>[0], "fetch">
 
 /**
- * Creates a server for the client using Hono.serve under the hood
- * @param client The Carbon client to create the server for
+ * Creates a server for the client or client manager using Hono.serve under the hood
+ * @param client The Carbon client or client manager to create the server for
  * @param options Additional options for the server
  * @returns The server instance
  */
-export function createServer(client: Client, options: ServerOptions): Server {
+export function createServer(
+	client: Client | ClientManager,
+	options: ServerOptions
+): Server {
 	const fetch = createHandler(client)
 	return Hono.serve({
 		// Weird type issue with options.createServer ??
